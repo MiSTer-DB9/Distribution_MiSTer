cf93e1ce06df55f98f0b5f14d0f485d27f737189
José Manuel Barroso Galindo <theypsilon@gmail.com>
2022-12-17 16:51:17 +0100

    Fix.

---
+++ .github/download_encc_distribution.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# Copyright (c) 2022 José Manuel Barroso Galindo <theypsilon@gmail.com>
+
+import json
+import time
+import sys
+import shutil
+import io
+import sys
+import configparser
+from pathlib import Path
+from download_distribution import process_all, classify_extra_content, fetch_extra_content_urls, fetch_cores, fetch_text, validate_cores, validate_extra_content_urls
+
+def main():
+
+    start = time.time()
+
+    cores = fetch_cores()
+    extra_content_urls = fetch_extra_content_urls()
+    extra_content_categories = classify_extra_content(extra_content_urls)
+    forks = fetch_forks()
+
+    print(f'Cores {len(cores)}:')
+    print(json.dumps(cores))
+    print()
+
+    validate_cores(cores)
+
+    print(f'Extra Content URLs {len(extra_content_urls)}:')
+    print(json.dumps(extra_content_urls))
+    print()
+
+    validate_extra_content_urls(extra_content_urls)
+
+    print('Extra Content Categories:')
+    print(json.dumps(extra_content_categories))
+    print()
+
+    print('Forks:')
+    print(json.dumps(forks))
+    print()
+
+    replace_core_urls(cores, forks)
+
+    target = 'delme'
+    if len(sys.argv) > 1:
+        target = sys.argv[1].strip()
+
+    if 'delme' in target.lower():
+        shutil.rmtree(target, ignore_errors=True)
+        Path(target).mkdir(parents=True, exist_ok=True)
+
+    process_all(extra_content_categories, cores, target)
+
+    print()
+    print("Time:")
+    end = time.time()
+    print(end - start)
+    print()
+
+def fetch_forks():
+    with io.StringIO(fetch_text('https://raw.githubusercontent.com/MiSTer-DB9/Forks_MiSTer/master/Forks.ini')) as buf:
+        config = configparser.ConfigParser(inline_comment_prefixes=(';','#'))
+        config.read_file(buf)
+        forks = {}
+        for sec in config.sections():
+            forks[sec] = {}
+            for key, val in config.items(sec):
+                forks[sec][key] = val
+        return forks
+
+def replace_core_urls(cores, forks):
+    replacements = {}
+    for fork in forks['Forks']['syncing_forks'].split(' '):
+        upstream_repo = str(Path(forks[fork]['upstream_repo']).with_suffix("")).replace('https:/g', 'https://g')
+        fork_repo = str(Path(forks[fork]['fork_repo']).with_suffix("")).replace('https:/g', 'https://g')
+        replacements[upstream_repo.lower()] = fork_repo
+
+    for core in cores:
+        if core['url'].lower() in replacements:
+            core['url'] = replacements[core['url'].lower()]
+
+if __name__ == '__main__':
+    main()
